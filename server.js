f56045f42c1c476baab85b3d2104142b1bb319a6
marcelomedeirosjrdev <marcelomedeiiros_@hotmail.com>
2025-06-16 14:01:17 -0300

    backend finalizado com sucesso

--- server.js
+++ server.js
@@ -32,6 +32,39 @@ app.post('/usuarios',  async (req, res) => {
     res.status(201).json({user})
 })
 
+app.put('/usuarios/:id',  async (req, res) => { 
+
+    req.params.id
+
+   const user = await prisma.user.update({
+
+    where:{
+        id: req.params.id
+    },
+        data: {
+            email: req.body.email,
+            age: req.body.age,
+            name: req.body.name
+        }
+
+       
+    })
+     console.log(user)
+
+    res.status(201).json({user})
+})
+
+
+app.delete('/usuarios/:id', async (req, res) => {
+    await prisma.user.delete({
+        where:{
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({message:"Usuario deletado com sucesso"})
+})
+
 app.listen(3001)
 
 // marcelomedeiiros
